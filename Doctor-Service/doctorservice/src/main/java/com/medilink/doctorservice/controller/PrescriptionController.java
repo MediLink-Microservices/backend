f31ce052aadfedd291a9573a02a0367833e20c0a
Navoda Hasaranga <hasaranganavoda2025@gmail.com>
2026-04-15 12:33:28 +0530

    Doctor Management service has been updated add Origins cors to the Controllers because to overcome front end data passing cors issues fix so added origin to controllers

--- Doctor-Service/doctorservice/src/main/java/com/medilink/doctorservice/controller/PrescriptionController.java
+++ Doctor-Service/doctorservice/src/main/java/com/medilink/doctorservice/controller/PrescriptionController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/prescriptions")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class PrescriptionController {
 
     private final PrescriptionService service;
